768df52c09e88e2b2f98efcf0f28baa03eac340d
Haseeb Abu Baker <haseebabubaker7@gmail.com>
2026-09-24 21:00:49 +0200

    Allow Aqua House domain through Astro origin check

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,17 @@
 import { defineConfig } from 'astro/config';
-
 import node from '@astrojs/node';
 
 export default defineConfig({
   output: 'static',
 
   adapter: node({
-    mode: 'standalone'
-  })
+    mode: 'standalone',
+  }),
+
+  security: {
+    allowedDomains: [
+      { hostname: 'aquahouse.co.za' },
+      { hostname: 'www.aquahouse.co.za' },
+    ],
+  },
 });
